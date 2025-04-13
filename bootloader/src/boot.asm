ffd84f6d5d699e01bf1ffa075abe27352e7e9f62
Ayush Jaiswal <ayushjaiswal220112096@gmail.com>
2025-04-13 17:09:04 +0530

    Completed Bootloader development with a sample empty kernel

--- bootloader/src/boot.asm
+++ bootloader/src/boot.asm
@@ -4,14 +4,30 @@
 
 CODE_OFFSET equ 0x8
 DATA_OFFSET equ 0x10
+
+KERNEL_LOOAD_SEG equ 0x1000
+KERNEL_START_ADDR equ 0x100000
 start:
     cli         ; Clear interrupts
-    xor ax, ax  ; Set AX to 0
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7C00
+    mov ax, 0x00  ; Set AX to 0
+    mov ds, ax    ; set data segment to 0x00
+    mov es, ax    ; Set extra segment -> 0x00
+    mov ss, ax    ; stack segment -> 0x00
+    mov sp, 0x7C00 ; set stack pointer to 0x7C00, top of bootloader segment
     sti         ; Enable interrupts
+
+; Load Kernel
+mov bx, KERNEL_LOOAD_SEG
+mov dh, 0x00
+mov dl, 0x80
+mov cl, 0x02
+mov ch, 0x00
+mov ah, 0x02
+mov al, 8
+int 0x13
+
+jc disk_read_error
+
 
 load_PM:
     cli
@@ -21,6 +37,10 @@ load_PM:
     mov cr0, eax
     jmp CODE_OFFSET:PModeMain
 
+disk_read_error:
+    hlt
+
+;gdt implementation
 gdt_start:
     dd 0x0
     dd 0x0
@@ -31,6 +51,7 @@ gdt_start:
     db 0x00   ; Base
     db 10011010b ; Access Byte
     db 11001111b ; Flags 
+    db 0x00      ; base
 
     ; Data Segment Descriptor
     dw 0xFFFF ; Limit
@@ -38,6 +59,7 @@ gdt_start:
     db 0x00   ; Base
     db 10010010b ; Access Byte
     db 11001111b ; Flags 
+    db 0x00      ; Base 
 
 gdt_end:
 
@@ -49,7 +71,8 @@ gdt_descriptor:
 [BITS 32]
 PModeMain:
     mov ax, DATA_OFFSET
-    mov ex, ax
+    mov ds, ax
+    mov es, ax
     mov fs, ax
     mov ss, ax
     mov gs, ax
@@ -60,7 +83,7 @@ PModeMain:
     or al, 2
     out 0x92, al
 
-    jmp $
+    jmp CODE_OFFSET:KERNEL_START_ADDR
 
 times 510 - ($ - $$) db 0
 dw 0xAA55  ; Boot sector signature
